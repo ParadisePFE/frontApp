d723e212919011d7d3e5c4f4462111f5edc56b83
Jemmoudi <me.jemmoudi@yaho.com>
2015-05-15 23:49:13 +0100

    integration de la vue principale

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
+// RUN THE FOUNDATION FRAMEWORK
 $(document).foundation();
+
+// RUH SLICK SLDER FOR CATEGORIES NAVIGATION
+$('#category-slick').owlCarousel({
+	items: 9,
+	nav: true,
+	center: true,
+	loop: true,
+	dots: false,
+	navText: ["", ""],
+	navContainer: '#slide-nav'
+});
+
+// RESET ITEMS WIDTH MANUALY
+$('.owl-item').css('width', 'auto');
